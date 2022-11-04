e84da8d18a4267c21ad38327383b1291b8d494e8
Mitchell R <mitchell.robert@bettercorp.co.za>
2022-11-04 20:03:36 +0200

    Updated install script

--- scripts/install.js
+++ scripts/install.js
@@ -1,4 +1,4 @@
-console.log('BetterPortal NPM Post Install Script');
+console.log("BetterPortal NPM Post Install Script");
 
 const fs = require("fs");
 const path = require("path");
@@ -6,14 +6,14 @@ const path = require("path");
 const cwd = process.env.INIT_CWD || process.cwd();
 const BPSDK_UI_DIR = path.join(
   cwd,
-  "./node_modules/@bettercorp/service-base-plugin-betterportal"
+  "./node_modules/@bettercorp/service-base-plugin-betterportal/ui"
 );
 
 const packageJsonFile = path.join(cwd, "./package.json");
 let packageJSON = JSON.parse(fs.readFileSync(packageJsonFile).toString());
 
 if (packageJSON.name === "@bettercorp/service-base-plugin-betterportal") return;
-console.log('BetterPortal NPM Post Install Script : Run install/update');
+console.log("BetterPortal NPM Post Install Script : Run install/update");
 
 const uiDir = path.join(cwd, "./ui");
 if (!fs.existsSync(uiDir)) fs.mkdirSync(uiDir);
@@ -38,9 +38,9 @@ packageJSON.scripts.start =
 packageJSON.scripts["build-ui"] = "cd ui; npm ci; npm run build; cd ../";
 
 packageJSON.files = packageJSON.files || [];
-if (packageJSON.files.indexOf("lib/**/*") >= 0)
+if (packageJSON.files.indexOf("lib/**/*") < 0)
   packageJSON.files.push("lib/**/*");
-if (packageJSON.files.indexOf("bpui/**/*") >= 0)
+if (packageJSON.files.indexOf("bpui/**/*") < 0)
   packageJSON.files.push("bpui/**/*");
 
 fs.writeFileSync(packageJsonFile, JSON.stringify(packageJSON, " ", 2));
