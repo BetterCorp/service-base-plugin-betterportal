49ba831e6da861b01da467c7357513a87192047b
Mitchell R <mitchell.robert@bettercorp.co.za>
2022-11-05 09:55:57 +0200

    Updated install script. Also changed dir for TS work

--- scripts/install.js
+++ scripts/install.js
@@ -2,6 +2,8 @@ console.log("BetterPortal NPM Post Install Script");
 
 const fs = require("fs");
 const path = require("path");
+const os = require("os");
+const child_process = require("child_process");
 
 const cwd = process.env.INIT_CWD || process.cwd();
 const BPSDK_UI_DIR = path.join(
@@ -15,11 +17,13 @@ let packageJSON = JSON.parse(fs.readFileSync(packageJsonFile).toString());
 if (packageJSON.name === "@bettercorp/service-base-plugin-betterportal") return;
 console.log("BetterPortal NPM Post Install Script : Run install/update");
 
-const uiDir = path.join(cwd, "./ui");
+const uiDir = path.join(cwd, "./betterportal-ui");
 if (!fs.existsSync(uiDir)) fs.mkdirSync(uiDir);
 
 const uiSrcDir = path.join(uiDir, "./src");
 if (!fs.existsSync(uiSrcDir)) fs.mkdirSync(uiSrcDir);
+const uiSrcAssetsDir = path.join(uiSrcDir, "./assets");
+if (!fs.existsSync(uiSrcAssetsDir)) fs.mkdirSync(uiSrcAssetsDir);
 
 for (let file of fs.readdirSync(BPSDK_UI_DIR, { withFileTypes: true })) {
   if (file.isDirectory()) continue;
@@ -29,13 +33,14 @@ for (let file of fs.readdirSync(BPSDK_UI_DIR, { withFileTypes: true })) {
   );
 }
 
+const bpBuildCmd = "cd betterportal-ui; npm ci; npm run build; cd ../";
 packageJSON.scripts = packageJSON.scripts || {};
-packageJSON.scripts.build = "tsc";
-packageJSON.scripts.dev =
-  "nodemon --config node_modules/@bettercorp/service-base/development/nodemon.json";
-packageJSON.scripts.start =
-  "ts-node node_modules/@bettercorp/service-base/lib/cli.js";
-packageJSON.scripts["build-ui"] = "cd ui; npm ci; npm run build; cd ../";
+packageJSON.scripts.build = "tsc ; " + bpBuildCmd;
+// packageJSON.scripts.dev =
+//   "nodemon --config node_modules/@bettercorp/service-base/development/nodemon.json";
+// packageJSON.scripts.start =
+//   "ts-node node_modules/@bettercorp/service-base/lib/cli.js";
+//packageJSON.scripts["build-ui"] = "cd betterportal-ui; npm ci; npm run build; cd ../";
 
 packageJSON.files = packageJSON.files || [];
 if (packageJSON.files.indexOf("lib/**/*") < 0)
@@ -44,3 +49,14 @@ if (packageJSON.files.indexOf("bpui/**/*") < 0)
   packageJSON.files.push("bpui/**/*");
 
 fs.writeFileSync(packageJsonFile, JSON.stringify(packageJSON, " ", 2));
+
+const gitIgnoreFile = path.join(cwd, ".gitignore");
+let gitignore = fs.readFileSync(gitIgnoreFile).toString().split(os.EOL);
+if (gitignore.indexOf("/lib") < 0) gitignore.push("/lib");
+if (gitignore.indexOf("/bpui") < 0) gitignore.push("/bpui");
+if (gitignore.indexOf("/betterportal/node_modules") < 0)
+  gitignore.push("/betterportal/node_modules");
+
+fs.writeFileSync(gitIgnoreFile, gitignore.join(os.EOL));
+
+child_process.exec('cd "' + uiDir + '"; npm ci');
